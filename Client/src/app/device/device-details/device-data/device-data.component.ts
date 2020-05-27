d400588ea2a040f12c313c8c89e21d615441df5c
esteban-gs <comsat61@gmail.com>
2020-05-26 20:03:02 -0700

    Addde a view/edit component

--- Client/src/app/device/device-details/device-data/device-data.component.ts
+++ Client/src/app/device/device-details/device-data/device-data.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { DeviceForDetails } from '../../../_interface/device-for-details';
 import { Router } from '@angular/router';
 import { ActionsService } from '../../actions.service';
+import { DeviceService } from '../../../shared/device.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Location, CurrencyPipe } from '@angular/common';
+import { DeviceForCreate } from '../../../_interface/device-for-create';
+import { MatDialog } from '@angular/material/dialog';
+import { SuccessDialogComponent } from 'src/app/shared/dialogs/success-dialog/success-dialog.component';
+import { ErrorHandlerService } from '../../../shared/error-handler.service';
 
 @Component({
   selector: 'app-device-data',
@@ -10,6 +17,8 @@ import { ActionsService } from '../../actions.service';
 })
 export class DeviceDataComponent implements OnInit {
   private dialogConfig;
+  public deviceForm: FormGroup;
+  maxDate: Date;
 
   @Input() public device: DeviceForDetails;
   @Output() selectEmitt = new EventEmitter();
@@ -17,10 +26,16 @@ export class DeviceDataComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private actionsServ: ActionsService
+    private actionsServ: ActionsService,
+    private repoService: DeviceService,
+    private location: Location,
+    private dialog: MatDialog,
+    private errorService: ErrorHandlerService
   ) { }
 
   ngOnInit() {
+    this.createForm();
+    this.deviceForm.disable();
     this.dialogConfig = {
       height: 'auto',
       width: 'auto',
@@ -29,13 +44,25 @@ export class DeviceDataComponent implements OnInit {
     };
   }
 
+  public createForm() {
+    this.deviceForm = new FormGroup({
+      name: new FormControl(this.device.name, [Validators.required, Validators.maxLength(365)]),
+      description: new FormControl(this.device.description, [Validators.maxLength(500)]),
+      purchased: new FormControl(this.device.purchased, [Validators.required]),
+      value: new FormControl(this.device.value, [Validators.required]),
+      productId: new FormControl(this.device.productId, [Validators.required, Validators.maxLength(20)]),
+      categoryId: new FormControl(1),
+      makerId: new FormControl(1),
+      // Not handling creating devices with employeeDevices assigned. Will handle on edit
+      employeesIds: new FormControl([]),
+    });
+  }
   public onChange = (event) => {
     this.selectEmitt.emit(event.value);
   }
 
-  public redirectToEdit = (id: string) => {
-    const url = `/device/edit/${id}`;
-    this.router.navigate([url]);
+  public toggleEdit = () => {
+    this.deviceForm.enable();
   }
 
   public redirectToList = () => {
@@ -47,5 +74,53 @@ export class DeviceDataComponent implements OnInit {
     this.actionsServ.delete(id, this.dialogConfig);
   }
 
+  public hasError = (controlName: string, errorName: string) => {
+    return this.deviceForm.controls[controlName].hasError(errorName);
+  }
+
+  public onCancel = () => {
+    this.deviceForm.reset();
+    this.createForm();
+    this.deviceForm.disable();
+    const url = `/device/details/${this.device.id}`;
+    this.router.navigate([url]);
+  }
+
+  public createDevice = (deviceFormValue) => {
+    if (this.deviceForm.valid) {
+      this.executeOwnerCreation(deviceFormValue);
+    }
+  }
+
+  // employees should be assigned after creation
+  private executeOwnerCreation = (deviceFormValue) => {
+    const device: DeviceForCreate = {
+      name: deviceFormValue.name,
+      description: deviceFormValue.description,
+      purchased: deviceFormValue.purchased,
+      value: deviceFormValue.value,
+      productId: deviceFormValue.productId,
+      categoryId: deviceFormValue.categoryId,
+      makerId: deviceFormValue.makerId,
+      employeesIds: []
+    };
+
+    const apiUrl = 'api/devices';
+    this.repoService.create(apiUrl, device)
+      .subscribe(res => {
+        const dialogRef = this.dialog.open(SuccessDialogComponent, this.dialogConfig);
+
+        // subscribing on the [mat-dialog-close] attribute as soon as dialog button is clicked
+        dialogRef.afterClosed()
+          .subscribe(result => {
+            this.location.back();
+          });
+      },
+        (error => {
+          this.errorService.dialogConfig = { ...this.dialogConfig };
+          this.errorService.handleError(error);
+        })
+      );
+  }
 
 }
